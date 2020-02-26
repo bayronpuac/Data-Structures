3e6fa7a4c2d30013c5e97c662dfd5e8c43f0aea9
Bayron Puac <bayronpuac@gmail.com>
2020-02-26 16:50:26 -0600

    Finsihed the contain

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,12 +12,33 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value) 
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -27,7 +48,6 @@ class BinarySearchTree:
     # You may use a recursive or iterative approach
     def for_each(self, cb):
         pass
-
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -20,3 +20,4 @@ class Stack:
 
     def len(self):
         return self.size
+
